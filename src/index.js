2fecbc7eb09ac3bd453e3db5a14d4805f5007a50
jairodias <jairopereira_dias@hotmail.com>
2021-11-03 17:07:47 -0300

    criando rota para editar dados da lista de to-do

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ app.use(express.json());
 
 const users = [];
 
+function checkExistsUsername(request, response, next) {
+    const { username } = request.headers;
+
+    const userAlreadyExists = users.find(user => user.username == username);
+
+    if (!userAlreadyExists) {
+        return response.status(400).json({
+            error: "Username not exists."
+        });
+    }
+
+    request.user = userAlreadyExists;
+
+    return next();
+}
+
 app.post('/users', (request, response) => {
     const { name, username } = request.body;
 
@@ -34,16 +50,56 @@ app.post('/users', (request, response) => {
     });
 });
 
-app.post('/todos', (request, response) => {
+app.post('/todos', checkExistsUsername, (request, response) => {
+    const { user } = request;
+    const { title, deadline } = request.body;
 
+    const todo = {
+        id: uuid(),
+        done: false,
+        title,
+        deadline: new Date(deadline),
+        creadted_at: new Date()
+    }
+
+    user.todos.push(todo);
+
+    return response.status(201).json({
+        success: true,
+        data: user.todos
+    });
 });
 
-app.get('/todos', (request, response) => {
+app.get('/todos', checkExistsUsername, (request, response) => {
+    const { user } = request;
 
+    return response.status(200).json(user.todos);
 });
 
-app.put('/todos/:id', (request, response) => {
+app.put('/todos/:id', checkExistsUsername, (request, response) => {
+    const { title, deadline } = request.body;
+    const { id } = request.params;
+    const { user } = request;
 
+    const indexTodos = user.todos.findIndex(todo => todo.id == id);
+
+    if (indexTodos < 0) {
+        return response.status(400).json({
+            error: "Task does not exist."
+        });
+    }
+
+    user.todos[indexTodos] = {
+        id: user.todos[indexTodos].id,
+        title,
+        deadline: new Date(deadline),
+        created: user.todos[indexTodos].created_at
+    }
+
+    return response.status(200).json({
+        success: true,
+        data: user.todos
+    });
 });
 
 app.listen(3000);
